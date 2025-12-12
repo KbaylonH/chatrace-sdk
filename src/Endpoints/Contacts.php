f70ea8072aef15aa1b9a570e47a7a6d74dea3539
Kevin William Baylón Huerta <kbaylonh@outlook.com>
2025-12-12 16:55:14 -0500

    fix:

--- src/Endpoints/Contacts.php
+++ src/Endpoints/Contacts.php
@@ -20,19 +20,43 @@ class Contacts
 
     public function getByPhone(string $phone)
     {
-        return $this->client->get("contacts/find_by_custom_field", [
+        $response = $this->client->get("contacts/find_by_custom_field", [
             'field_id' => 'phone',
             'value'    => $phone,
         ]);
+
+        if($response->successful()){
+            $result = $response->json();
+            if(!empty($result['data'])){
+                return $result['data'][0];
+            }
+        }
+
+        throw new \Exception("Contact not found");
     }
 
     public function create(array $data)
     {
-        return $this->client->post("contacts", $data);
+        $response = $this->client->post("contacts", $data);
+        if($response->successful()){
+            $result = $response->json();
+            if($result['success'] && !empty($result['data'])){
+                return $result['data'];
+            }
+        }
+
+        throw new \Exception("Failed to create contact");
     }
 
     public function sendContent($id, $data){
-        return $this->client->post("contacts/{$id}/send_content", $data);
+        $response = $this->client->post("contacts/{$id}/send_content", $data);
+
+        if($response->successful()){
+            $result = $response->json();
+            return $result['success'];
+        }
+
+        throw new \Exception("Failed to send content to contact");
     }
 
     public function sendText($id, string $text, string $channel = 'omnichannel'){
